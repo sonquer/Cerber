0bf42d33dc5566c7a7f54c3ea7d2fe1067b94308
kamilagruhlke <kamilagruhlke@gmail.com>
2020-06-11 19:48:19 +0200

    Added pages

--- Mobile/Cerber/App.xaml.cs
+++ Mobile/Cerber/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cerber.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Cerber
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ServicesListPage();
         }
 
         protected override void OnStart()
---
+++ Mobile/Cerber/Views/AttachServicesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cerber.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AttachServicesPage : ContentPage
+    {
+        public AttachServicesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
