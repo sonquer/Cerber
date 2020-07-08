dc4bc8caffbdc891022d52e1c5569af2d759b13d
kamilagruhlke <kamilagruhlke@gmail.com>
2020-07-08 21:32:48 +0200

    Create service page

---
+++ Mobile/Cerber/Models/CreateServiceModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Cerber.Models
+{
+    public class CreateServiceModel
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public int ExpectedStatusCode { get; set; }
+
+        public string ExpectedResponse { get; set; }
+
+        public int LifetimeInHours { get; set; }
+    }
+}
--- Mobile/Cerber/Views/AttachServicePage.xaml.cs
+++ Mobile/Cerber/Views/AttachServicePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using Cerber.Models;
+using Cerber.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Cerber.Views
@@ -6,9 +8,25 @@ namespace Cerber.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AttachServicesPage : ContentPage
     {
+        private AttachServiceViewModel _viewModel => BindingContext as AttachServiceViewModel;
+
         public AttachServicesPage()
         {
             InitializeComponent();
+
+            BindingContext = new AttachServiceViewModel(Navigation);
+        }
+
+        private void Create_Service_Clicked(object sender, System.EventArgs e)
+        {
+            _viewModel.SaveAvailabilityRecordCommand.Execute(new CreateServiceModel
+            {
+                Name = name.Text,
+                Url = url.Text,
+                ExpectedResponse = expectedResponse.Text,
+                ExpectedStatusCode = int.Parse(expectedStatusCode.Text ?? "200"),
+                LifetimeInHours = int.Parse(logLifetimeInHours.Text ?? "1")
+            });
         }
     }
 }
